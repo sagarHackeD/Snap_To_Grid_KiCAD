b57be298b437a728cccfed47deea5380527a6dca
sagarHackeD <sagarnaik430@googlemail.com>
2024-06-27 22:41:10 +0530

    pylint

--- snapToGrid/__init__.py
+++
@@ -1,5 +0,0 @@
-# This file is executed when the package is imported (on PCB editor startup)
-
-from .snap_to_grid import SnapToGrid # Note the relative import!
-
-SnapToGrid().register() # Instantiate and register to PCB editor
